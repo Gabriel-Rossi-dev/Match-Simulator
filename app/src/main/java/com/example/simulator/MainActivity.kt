cfdf431c2832efa7c4e54954066aa53a7cec7bf5
Gabriel-Rossi-dev <costagabrielrd@gmail.com>
2022-06-21 13:33:30 -0300

    icon create

--- app/src/main/java/com/example/simulator/MainActivity.kt
+++ app/src/main/java/com/example/simulator/MainActivity.kt
@@ -2,10 +2,18 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.simulator.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
     }
 }
